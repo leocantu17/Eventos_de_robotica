34312ec8dfcba866a71cdb0288dce43cb19e193d
leocantu17 <140271635+leocantu17@users.noreply.github.com>
2023-10-07 22:11:45 -0600

    Inicio del proyecto

---
+++ Evento_robotica/db/db.js
@@ -0,0 +1,15 @@
+const { sql, sqlConn } = require("../config/conexion");
+const { fecha } = require("../extras/fecha");
+let date = fecha();
+
+const objetoResultadoSQL = (resultado) => {
+  if (resultado.recordsets.length > 1) {
+    return { estatus: "OK", datos: resultado.recordsets };
+  }
+
+  if (!resultado.recordset) {
+    return { estatus: "OK", datos: [] };
+  }
+
+  return { estatus: "OK", datos: resultado.recordset };
+};
